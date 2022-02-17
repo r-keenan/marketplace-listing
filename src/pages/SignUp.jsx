8d4840754af77247ff74a514f21fb952ffdb5472
Ross Keenan <ross.keenan@protonmail.com>
2022-02-17 15:25:57 -0600

    Enabled user to sign up and user becomes part of Firestore auth table.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visiblityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -23,6 +29,30 @@ function SignUp() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -30,7 +60,7 @@ function SignUp() {
           <p className="pageHeader">Welcome Back!</p>
 
           <main>
-            <form>
+            <form onSubmit={onSubmit}>
               <input
                 type="text"
                 className="nameInput"
