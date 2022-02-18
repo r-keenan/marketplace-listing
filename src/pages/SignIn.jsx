744f6d34d747eb650a1e8bc2d3ae9816ac8f87d5
Ross Keenan <ross.keenan@protonmail.com>
2022-02-17 18:00:23 -0600

    Implemented signing up with Oauth.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visiblityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -87,7 +88,7 @@ function SignIn() {
                 </button>
               </div>
             </form>
-            {/*Google OAuth */}
+            <OAuth />
             <Link to="/sign-up" className="registerLink">
               Sign Up Instead
             </Link>
