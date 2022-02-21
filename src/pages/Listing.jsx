792d4c978d9b97190ce24ccf8b056a1b43963997
Ross Keenan <ross.keenan@protonmail.com>
2022-02-21 16:56:57 -0600

    Created Listing page. Omitted map portion, because I am not using Geocaching.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,6 +6,11 @@ import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
 
+const formatPrice = (price) => {
+  let formattedPrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return formattedPrice;
+};
+
 function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -30,7 +35,73 @@ function Listing() {
     fetchListing();
   }, [navigate, params.listingId]);
 
-  return <div>Listing</div>;
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <main>
+      {/* Slider */}
+      {/*Copy to clipboard: navigator.clipboard.writeText(window.location.href) */}
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="Share" />
+      </div>
+
+      {shareLinkCopied && <p className="linkCopied">Linked Copied!</p>}
+
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $
+          {listing.offer
+            ? formatPrice(listing.discountedPrice)
+            : formatPrice(listing.regularPrice)}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${formatPrice(listing.regularPrice - listing.discountedPrice)}{" "}
+            discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?ListingName=${listing.name}&listingLocation=${listing.location}`}
+            className="primaryButton"
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default Listing;
