1c592df3be16a7d474d68472293f8766044fb076
Ross Keenan <ross.keenan@protonmail.com>
2022-02-21 19:34:00 -0600

    Created swiper component and import into the Explore page.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,6 +8,7 @@ import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
+
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const formatPrice = (price) => {
